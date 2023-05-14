10884950759d1c81e6c15725e27298ddb976bbf7
Esteban Torres <me+github@estebantorr.es>
2023-05-14 17:19:33 +0200

    Makes the DB code 'db agnostic' By using gorm the code is not tied to a specific database and can be easily swapped for any DB as required.

--- db_test.go
+++ db_test.go
@@ -4,40 +4,88 @@
 package golink
 
 import (
-	"path"
+	"database/sql"
+	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
+	cmpopts "github.com/google/go-cmp/cmp/cmpopts"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-// Test saving, loading, and deleting links for SQLiteDB.
-func Test_SQLiteDB_SaveLoadDeleteLinks(t *testing.T) {
-	db, err := NewSQLiteDB(path.Join(t.TempDir(), "links.db"))
+// Test saving, loading, and deleting links for DB.
+func Test_DB_SaveLoadDeleteLinks(t *testing.T) {
+	var (
+		sqldb *sql.DB
+		err   error
+		mock  sqlmock.Sqlmock
+		db    *gorm.DB
+	)
+	sqldb, mock, err = sqlmock.New()
+	if err != nil {
+		t.Errorf("Unable to mock DB connection. %e", err)
+	}
+
+	db, err = gorm.Open(mysql.New(mysql.Config{
+		DriverName:                "mysql",
+		Conn:                      sqldb,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{SkipDefaultTransaction: true})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	SUT, err := newDB(db)
 	if err != nil {
 		t.Error(err)
 	}
 
 	links := []*Link{
-		{Short: "short", Long: "long"},
-		{Short: "Foo.Bar", Long: "long"},
+		{Short: "short", Long: "long", ID: linkID("short")},
+		{Short: "Foo.Bar", Long: "long", ID: linkID("Foo.Bar")},
 	}
 
+	mock.MatchExpectationsInOrder(false)
 	for _, link := range links {
-		if err := db.Save(link); err != nil {
+		time := sqlmock.AnyArg()
+		mock.ExpectExec(regexp.QuoteMeta(
+			"UPDATE `links` SET `created_at`=?,`updated_at`=?,`deleted_at`=?,`short`=?,`long`=?,`created`=?,`last_edit`=?,`owner`=? WHERE `links`.`deleted_at` IS NULL AND `id` = ?")).
+			WithArgs(time, sqlmock.AnyArg(), nil, link.Short, link.Long, time, time, "", linkID(link.Short)).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+
+		mock.ExpectQuery(regexp.QuoteMeta(
+			"SELECT * FROM `links` WHERE id = ? AND `links`.`deleted_at` IS NULL ORDER BY `links`.`id` LIMIT 1")).
+			WithArgs(linkID(link.Short)).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "short", "long", "created_at", "last_edit"}).
+				AddRow(linkID(link.Short), link.Short, link.Long, link.CreatedAt, link.LastEdit))
+
+		if err := SUT.Save(link); err != nil {
 			t.Error(err)
 		}
-		got, err := db.Load(link.Short)
+
+		got, err := SUT.Load(link.Short)
 		if err != nil {
 			t.Error(err)
 		}
 
-		if !cmp.Equal(got, link) {
-			t.Errorf("db save and load got %v, want %v", *got, *link)
+		if !cmp.Equal(got, link, cmpopts.IgnoreFields(Link{}, "Created", "LastEdit", "UpdatedAt")) {
+			t.Errorf("db save and load got %+v, want %+v", *got, *link)
 		}
 	}
 
-	got, err := db.LoadAll()
+	selected_rows := sqlmock.NewRows([]string{"id", "short", "long", "created_at", "last_edit"})
+	for _, link := range links {
+		selected_rows.AddRow(linkID(link.Short), link.Short, link.Long, link.CreatedAt, link.LastEdit)
+	}
+
+	mock.ExpectQuery(regexp.QuoteMeta(
+		"SELECT * FROM `links` WHERE `links`.`deleted_at` IS NUL")).
+		WillReturnRows(selected_rows)
+
+	got, err := SUT.LoadAll()
 	if err != nil {
 		t.Error(err)
 	}
@@ -45,29 +93,67 @@ func Test_SQLiteDB_SaveLoadDeleteLinks(t *testing.T) {
 	sortLinks := cmpopts.SortSlices(func(a, b *Link) bool {
 		return a.Short < b.Short
 	})
-	if !cmp.Equal(got, links, sortLinks) {
-		t.Errorf("db.LoadAll got %v, want %v", got, links)
+
+	if !cmp.Equal(got, links, sortLinks, cmpopts.IgnoreFields(Link{}, "Created", "LastEdit", "UpdatedAt")) {
+		t.Errorf("db.LoadAll got %+v, want %+v", got, links)
 	}
 
 	for _, link := range links {
-		if err := db.Delete(link.Short); err != nil {
+		mock.ExpectExec(regexp.QuoteMeta(
+			"UPDATE `links` SET `deleted_at`=? WHERE `links`.`id` = ? AND `links`.`deleted_at` IS NULL")).
+			WithArgs().
+			WillReturnResult(sqlmock.NewResult(0, 1))
+
+		if err := SUT.Delete(link.Short); err != nil {
 			t.Error(err)
 		}
 	}
 
-	got, err = db.LoadAll()
+	mock.ExpectQuery(regexp.QuoteMeta(
+		"SELECT * FROM `links` WHERE `links`.`deleted_at` IS NUL")).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "short", "long", "created_at", "last_edit"}))
+
+	got, err = SUT.LoadAll()
 	if err != nil {
 		t.Error(err)
 	}
+
 	want := []*Link(nil)
-	if !cmp.Equal(got, want) {
-		t.Errorf("db.LoadAll got %v, want %v", got, want)
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("db.LoadAll got %+v, want %+v", got, want)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
 	}
 }
 
-// Test saving, loading, and deleting stats for SQLiteDB.
-func Test_SQLiteDB_SaveLoadDeleteStats(t *testing.T) {
-	db, err := NewSQLiteDB(path.Join(t.TempDir(), "links.db"))
+// Test saving, loading, and deleting stats for DB.
+func Test_DB_SaveLoadDeleteStats(t *testing.T) {
+	var (
+		sqldb *sql.DB
+		err   error
+		mock  sqlmock.Sqlmock
+		db    *gorm.DB
+	)
+	sqldb, mock, err = sqlmock.New()
+	if err != nil {
+		t.Errorf("Unable to mock DB connection. %e", err)
+	}
+
+	db, err = gorm.Open(mysql.New(mysql.Config{
+		DriverName:                "mysql",
+		Conn:                      sqldb,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{SkipDefaultTransaction: true})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	mock.MatchExpectationsInOrder(false)
+
+	SUT, err := newDB(db)
 	if err != nil {
 		t.Error(err)
 	}
@@ -77,8 +163,14 @@ func Test_SQLiteDB_SaveLoadDeleteStats(t *testing.T) {
 		{Short: "a"},
 		{Short: "B-c"},
 	}
+
+	time := sqlmock.AnyArg()
 	for _, link := range links {
-		if err := db.Save(link); err != nil {
+		mock.ExpectExec(regexp.QuoteMeta(
+			"UPDATE `links` SET `created_at`=?,`updated_at`=?,`deleted_at`=?,`short`=?,`long`=?,`created`=?,`last_edit`=?,`owner`=? WHERE `links`.`deleted_at` IS NULL AND `id` = ?")).
+			WithArgs(time, time, nil, link.Short, link.Long, time, time, "", linkID(link.Short)).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+		if err := SUT.Save(link); err != nil {
 			t.Error(err)
 		}
 	}
@@ -97,12 +189,26 @@ func Test_SQLiteDB_SaveLoadDeleteStats(t *testing.T) {
 	}
 
 	for _, s := range stats {
-		if err := db.SaveStats(s); err != nil {
+		mock.ExpectBegin()
+		for id, click := range s {
+			mock.ExpectExec(regexp.QuoteMeta(
+				"UPDATE `stats` SET `created_at`=?,`updated_at`=?,`deleted_at`=?,`created`=?,`clicks`=? WHERE `stats`.`deleted_at` IS NULL AND `id` = ?")).
+				WithArgs(time, time, nil, time, click, linkID(id)).
+				WillReturnResult(sqlmock.NewResult(0, 1))
+		}
+		mock.ExpectCommit()
+		if err := SUT.SaveStats(s); err != nil {
 			t.Error(err)
 		}
 	}
 
-	got, err := db.LoadStats()
+	stats_rows := sqlmock.NewRows([]string{"id", "clicks"})
+	stats_rows = stats_rows.AddRow("a", 2)
+	stats_rows = stats_rows.AddRow("B-c", 3)
+	mock.ExpectQuery(regexp.QuoteMeta(
+		"SELECT ID, SUM(Clicks) as Clicks FROM `stats` WHERE `stats`.`deleted_at` IS NULL GROUP BY `id`")).
+		WillReturnRows(stats_rows)
+	got, err := SUT.LoadStats()
 	if err != nil {
 		t.Error(err)
 	}
@@ -111,17 +217,29 @@ func Test_SQLiteDB_SaveLoadDeleteStats(t *testing.T) {
 	}
 
 	for k := range want {
-		if err := db.DeleteStats(k); err != nil {
+		mock.ExpectExec(regexp.QuoteMeta(
+			"UPDATE `stats` SET `deleted_at`=? WHERE `stats`.`id` = ? AND `stats`.`deleted_at` IS NULL")).
+			WithArgs(sqlmock.AnyArg(), linkID(k)).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+
+		if err := SUT.DeleteStats(k); err != nil {
 			t.Error(err)
 		}
 	}
 
-	got, err = db.LoadStats()
+	mock.ExpectQuery(regexp.QuoteMeta(
+		"SELECT ID, SUM(Clicks) as Clicks FROM `stats` WHERE `stats`.`deleted_at` IS NULL GROUP BY `id`")).
+		WillReturnRows(sqlmock.NewRows([]string{"", ""}))
+	got, err = SUT.LoadStats()
 	if err != nil {
 		t.Error(err)
 	}
 	want = ClickStats{}
 	if !cmp.Equal(got, want) {
 		t.Errorf("db.LoadStats got %v, want %v", got, want)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
 	}
 }
